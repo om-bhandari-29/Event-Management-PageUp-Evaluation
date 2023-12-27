0856348164ff1337ed1d848183ce66de160a1925
ombhandari7989@gmail.com <ombhandari7989@gmail.com>
2023-12-27 13:34:24 +0530

    added database and port

--- index.js
+++ index.js
@@ -4,5 +4,34 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const morgan = require('morgan'); //to display route that is requested
 const path = require('path');
+const { connected } = require('process');
 
 dotenv.config();
+
+const port = process.env.PORT;
+const db = process.env.MONGO_DB_URL;
+
+app.listen(port, () =>{
+    console.log("App running on port : "+port);
+});
+
+mongoose.connect(db, {
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true
+}).then(()=>console.log("Database connected successfully"))
+  .catch((err) => console.log("Error occured while connecting to DB + "+err));
+
+/*
+app.connect(db, {
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true
+}).then(function(){
+    console.log("Database connected successfully");
+}).catch(function(err){
+    console.log("Error occured while connecting to DB + "+err);
+});
+*/
